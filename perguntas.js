8fc517eb7efba1191f6f94f9dcd0d024b9082f41
livsdead <livia.schebeuka@escola.pr.gov.br>
2024-09-24 07:59:09 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,46 @@
+criaCartao( //cria o cartao 
+    'Internet', //tópico da pergunta
+    'Quando a internet foi criada?', //pergunta
+    '29 de outubro de 1969, com o ARPANET.' //resposta
+)
+
+criaCartao(//cria o cartao 
+    'Geografia', //tópico da pergunta
+    'Qual a capital da França?', //pergunta
+    'A capital da França é Paris' //resposta
+)
+
+criaCartao( //cria o cartao 
+    'Programação', //tópico da pergunta
+    'O que é uma função?', //pergunta
+    'Uma função é um bloco de código que executa alguma tarefa' //resposta
+)
+
+criaCartao( //cria o cartao 
+    'Internet', //tópico da pergunta
+    'O que são cabos de transmissão intercontinentais?', //pergunta
+    '' //resposta
+)
+criaCartao( //cria o cartao 
+    'Programação', //tópico da pergunta
+    'O que é Python?', //pergunta
+    'O Python é uma linguagem de programação' //resposta
+)
+
+criaCartao(//cria o cartao 
+    'Geografia', //tópico da pergunta
+    'Qual a capital da França?', //pergunta
+    'A capital da França é Paris' //resposta
+)
+
+criaCartao( //cria o cartao 
+    'Programação', //tópico da pergunta
+    'O que é uma função?', //pergunta
+    'Uma função é um bloco de código que executa alguma tarefa' //resposta
+)
+
+criaCartao( //cria o cartao 
+    'Lingua inglesa', //tópico da pergunta
+    'Como se diz oi em Inglês?', //pergunta
+    'Oi em ingles é HI (RAI)' //resposta
+)
